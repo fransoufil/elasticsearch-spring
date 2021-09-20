4054a8de2d41d7cf71e2f865ad7f3afb5bc83bbf
fransoufil <fran.sf@uol.com.br>
2021-09-20 20:21:12 -0300

    Projeto criado

---
+++ src/main/java/com/fransoufil/springelasticsearchgradle/SpringElasticsearchGradleApplication.java
@@ -0,0 +1,13 @@
+package com.fransoufil.springelasticsearchgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticsearchGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElasticsearchGradleApplication.class, args);
+	}
+
+}
